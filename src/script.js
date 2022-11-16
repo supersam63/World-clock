1f5bd35dab49b27759f1d34dc1e3520e1758d97f
supersam63 <113955440+supersam63@users.noreply.github.com>
2022-11-16 11:16:19 -0700

    added js for date/time

---
+++ src/script.js
@@ -0,0 +1,35 @@
+function updateTime() {
+  let losAngelesElement = document.querySelector("#los-angeles");
+  let losAngelesDateElement = losAngelesElement.querySelector(".date");
+  let losAngelesTimeElement = losAngelesElement.querySelector(".time");
+  let losAngelesTime = moment().tz("America/Los_Angeles");
+
+  losAngelesDateElement.innerHTML = moment().format("MMMM Do YYYY");
+
+  losAngelesTimeElement.innerHTML = losAngelesTime.format(
+    "h:mm:ss [<small>]A[</small>]"
+  );
+
+  let parisElement = document.querySelector("#paris");
+  let parisDateElement = parisElement.querySelector(".date");
+  let parisTimeElement = parisElement.querySelector(".time");
+  let parisTime = moment().tz("Europe/Paris");
+
+  parisDateElement.innerHTML = moment().format("MMMM Do YYYY");
+
+  parisTimeElement.innerHTML = parisTime.format("h:mm:ss [<small>]A[</small>]");
+
+  let sydneyElement = document.querySelector("#sydney");
+  let sydneyDateElement = sydneyElement.querySelector(".date");
+  let sydneyTimeElement = sydneyElement.querySelector(".time");
+  let sydneyTime = moment().tz("Australia/Sydney");
+
+  sydneyDateElement.innerHTML = moment().format("MMMM Do YYYY");
+
+  sydneyTimeElement.innerHTML = sydneyTime.format(
+    "h:mm:ss [<small>]A[</small>]"
+  );
+}
+
+updateTime();
+setInterval(updateTime, 1000);
